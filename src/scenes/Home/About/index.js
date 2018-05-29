cd60449036654782838e32e830708f42aa64baf4
Vu Nguyen <its.vnguyen@gmail.com>
2018-05-28 22:34:34 -0500

    editing about

--- src/scenes/Home/About/index.js
+++ src/scenes/Home/About/index.js
@@ -4,17 +4,33 @@ import './style.css';
 class About extends Component {
   render() {
     return (
-      <div className="article-page">
-        <p>
-          On a daily basis, I work on complex front-end projects with up-to-date languages and technologies, and I also have a decent grasp of server-side development. 
-          While I enjoy experimenting with new frameworks and libraries, I understand the importance of placing focus on the end goal: delivering a beautiful, functional product to the user/client.
-        </p>
-        <p>I see every new experience as an opportunity to learn. 
-          My favorite projects are those that require me to push beyond my boundaries and acquire new skills and knowledge in order to succeed.
-        </p>
-        <p>
-          When I'm not behind a computer screen, I'm out there somewhere exploring, or staying at home drawing.
-        </p>
+      <div>
+        <div className="">
+          <h2>
+            I'm a developer currently based out of Dallas, TX. I focus on front-end, back-end, and side-ways development.
+          </h2>
+        </div>
+        <div className="article-page">
+          <p>
+            I've been in the software industry for over 5 years (maybe 10+ if you count that website I did in high school), and I'm now the Integrations Developer of Kickbox. 
+          </p>
+          <p>
+            In my day to day, I work with complex web projects, building and making sure the front-end is human-readable, taking more server-side tasks in writing models, libraries, middleware, and store procedures, and making time to read up/fiddle with new libraries and practices.
+          </p>
+          <p>
+            I believe being involved in all fields helps you stay curious and flexible.
+            Though I have been deeply learning towards everything front-end.
+            <pre lang="js " className="code">
+              // I love it.
+            </pre>
+          </p>
+          <p>
+            There's just something about creating responsive websites from scratch, being undecided on what tools to use in your stack, and spinning something up in seconds.
+          </p>
+          <p>
+            When I take off my developer hat, I'm usually out running around (literally) or trying to be an Overwatch pro.
+          </p>
+        </div>
       </div>
     );
   }
