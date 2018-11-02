954ee9ae3a630fb47597f0b2d2e15bae3383f8f9
Vu Nguyen <its.vnguyen@gmail.com>
2018-11-01 20:52:41 -0500

    cleaning, white is a bit better

--- src/scenes/Home/Contact/index.js
+++ src/scenes/Home/Contact/index.js
@@ -6,18 +6,6 @@ class Contact extends Component {
   render() {
     return (
       <section id="contact-section">
-        <div class="contact-section-wrapper fade-slide-up-element-2">
-          <p>
-            You can reach me for an inquiry or just to say hi at <br/>
-            <a href="mailto:hey.vuuu@gmail.com">hey.vuuu@gmail.com</a>.
-          </p>
-          <p>
-            For more about me, I'm also on <br />
-            <a href="" target="_blank">instagram</a> &amp; 
-            <a href="" target="_blank"> facebook</a> &amp; 
-            <a href="" target="_blank"> linkedin</a>.
-          </p>
-        </div>
       </section>
     )
   }
