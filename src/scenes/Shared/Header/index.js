0135169b16f0ad1a4c26018be0677c91178d219b
Vu Nguyen <its.vnguyen@gmail.com>
2020-04-27 22:09:19 -0500

    shared header init

---
+++ src/scenes/Shared/Header/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import {
+  useLocation,
+  useHistory
+} from "react-router-dom";
+
+import './style.scss';
+
+const SharedHeader = () => {
+  const location  = useLocation();
+  const history = useHistory();
+
+  const pathname = location.pathname;
+  console.log('location', location)
+
+  return (
+    <section className="shared-header-container">
+      <p onClick={() => history.push("/")} className={`${pathname === '/' ? 'active' : ''}`}>Work</p>
+      <p onClick={() => history.push("/resume")} className={`${pathname === '/resume' ? 'active' : ''}`}>Resume</p>
+      <p onClick={() => history.push("/blog")} className={`${pathname.includes('blog') ? 'active' : ''}`}>Blog</p>
+      <p onClick={() => history.push("/doodles")} className={`${pathname === '/doodles' ? 'active' : ''}`}>Doodles</p>
+    </section>
+  );
+};
+
+export default SharedHeader;
